d971444e1fc5433a13f6deccbe2224d7339d95d7
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-07 21:04:48 +0200

    Changed name of project to Flow, removed all references to old name.

---
+++ Flow.Core/Contracts/UserRepository.cs
@@ -0,0 +1,22 @@
+﻿using Flow.Core.DomainModels;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Flow.Core.Contracts
+{
+    public interface IUserRepository
+    {
+        Task CreateUser(User newUser);
+
+        /** Removes the user with the specified id, if it exists in the database. */
+        Task DeleteUser(Guid userId);
+
+        /** Returns a list of users that fulfill the provided predicate. */
+        Task<ICollection<User>> FindUsersAsync(Func<User, bool> predicate);
+
+        /** Retreive the suer with the specified id, or null if it does not exist. */
+        Task<User> GetUserById(Guid userId);
+    }
+}
---
+++ FlowUI/Pages/Counter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FLowUI.Pages
+{
+    public partial class Counter
+    {
+        private int CurrentCount = 0;
+
+        public Counter() {}
+
+        protected override async Task OnInitializedAsync()
+        {
+            CurrentCount = 0;
+        }
+
+        private void IncrementCount()
+        {
+            CurrentCount++;
+        }
+    }
+}
