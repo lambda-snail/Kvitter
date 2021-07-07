27c46f5d5b4e18f994df45199b3b1244547faf61
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-07 11:12:26 +0200

    Added a User class and a bare-bones repository class for User objects.

---
+++ Kvitter.Core/DomainModels/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kvitter.Core.DomainModels
+{
+    /**
+     * The class that represents a user in the system.
+     */
+    public class User
+    {
+        /** This user is linked to an IdentituUser via the user id, which is the same in both databases. */
+        public Guid UserId { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+        public String Description { get; set; }
+
+        //public ICollection<Post> Posts { get; set; }
+        //public ICollection<User> Friends { get; set; }
+        //public ICollection<User> BlockedUsers { get; set; }
+
+    }
+}
