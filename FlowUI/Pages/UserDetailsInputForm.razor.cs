9a971ce8e3316ece9c6980b0aa55575fdac6457d
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-09 10:45:02 +0200

    Created file to hold form for editing of user details.

---
+++ FlowUI/Pages/UserDetailsInputForm.razor.cs
@@ -0,0 +1,32 @@
+﻿using Flow.Core.DomainModels;
+using Flow.Core.Mediate.UserQuery;
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace FlowUI.Pages
+{
+    public partial class UserDetailsInputForm
+    {
+        [Inject]
+        public IMediator _mediator { get; set; }
+
+        [CascadingParameter]
+        protected Task<AuthenticationState> authenticationStateTask { get; set; }
+        private User LoggedInUser { get; set; }
+
+        public UserDetailsInputForm() { }
+        
+        protected override async Task OnInitializedAsync()
+        {
+            AuthenticationState authenticationState = await authenticationStateTask;
+            string userId = authenticationState.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            LoggedInUser = await _mediator.Send(new GetUserByIdRequest<User> { UserId = Guid.Parse(userId) });
+        }
+    }
+}
