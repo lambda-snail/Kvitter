9a9e054bf95590336d9e3da709fd860a3ff4a75e
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-09 10:44:18 +0200

    Added request and handler to obtain user by id.

---
+++ Flow.Core/Mediate/UserQuery/GetUserByIdRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System;
+
+namespace Flow.Core.Mediate.UserQuery
+{
+    public class GetUserByIdRequest<User> : IRequest<User>
+    {
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Flow.Core/Mediate/UserQuery/GetUserByIdRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using Flow.Core.Contracts;
+using Flow.Core.DomainModels;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flow.Core.Mediate.UserQuery
+{
+    public class GetUserByIdRequestHandler : IRequestHandler<GetUserByIdRequest<User>, User>
+    {
+        private readonly IUserRepository _repository;
+
+        public GetUserByIdRequestHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+        public Task<User> Handle(GetUserByIdRequest<User> request, CancellationToken cancellationToken)
+        {
+            return _repository.GetUserByIdAsync(request.UserId);
+        }
+    }
+}
