f5e02cd9c465e888e3da69c8f09572041d68895e
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-16 09:30:40 +0200

    Added a button and a modal dialog to create posts.

---
+++ FlowUI/Pages/NewPostForm.razor.cs
@@ -0,0 +1,52 @@
+﻿using Flow.Core.DomainModels;
+using System.Threading.Tasks;
+
+namespace FlowUI.Pages
+{
+    public partial class NewPostForm
+    {
+        public Post Post { get; set; } = new Post();
+
+        public bool ShowDialog { get; set; } = false;
+
+        protected override void OnInitialized()
+        {
+            ShowDialog = false;
+        }
+
+        public void HandleValidSubmit()
+        {
+            CloseDialog();
+        }
+
+        public void HandleInvalidSubmit()
+        {
+
+        }
+
+        public void OpenDialog()
+        {
+            if(!ShowDialog)
+            {
+                ResetDialog();
+                ShowDialog = true;
+                StateHasChanged();
+            }
+        }
+
+        public void CloseDialog()
+        {
+            if(ShowDialog)
+            {
+                ResetDialog();
+                ShowDialog = false;
+                StateHasChanged();
+            }
+        }
+
+        private void ResetDialog()
+        {
+            Post = new Post();
+        }
+    }
+}
---
+++ FlowUI/Pages/WorldFlow.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace FlowUI.Pages
+{
+    public partial class WorldFlow
+    {
+        public NewPostForm NewPostForm { get; set; }
+        public void OpenNewPostForm()
+        {
+            NewPostForm.OpenDialog();
+        }
+    }
+}
