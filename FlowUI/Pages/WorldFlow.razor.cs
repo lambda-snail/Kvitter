4137b4d7f8d78f6df150552f946bb9d977076664
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-17 15:37:00 +0200

    Added virtualized display and paged loading of posts from the DB.

--- FlowUI/Pages/WorldFlow.razor.cs
+++ FlowUI/Pages/WorldFlow.razor.cs
@@ -1,10 +1,38 @@
 ﻿//using Microsoft.AspNetCore.Components.Web.Virtualization;
 
+using Flow.Core.Contracts;
+using Flow.Core.DomainModels;
+using Flow.Core.Mediate.DatabaseSize;
+using Flow.Core.Mediate.GetPosts;
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using System.Threading.Tasks;
+
 namespace FlowUI.Pages
 {
     public partial class WorldFlow
     {
         public NewPostForm NewPostForm { get; set; }
+
+        [Inject]
+        protected IMediator _mediator { get; set; }
+
+        [Inject]
+        protected IPostRepository _repository { get; set; }
+
+        /**
+         * Things that might need to be changed if the application is large/has many users:
+         * - Handling the total count of posts (minimize number of requests).
+         * - Conversion from long to int might not always work.
+         */
+        public async ValueTask<ItemsProviderResult<Post>> LoadPostsPaged(ItemsProviderRequest request)
+        {
+            var posts = await _mediator.Send(new GettAllPostsPagedRequest { Skip=request.StartIndex, Take=request.Count });
+            var totalPostCount = await _mediator.Send(new EstimatePostCountRequest());
+            return new ItemsProviderResult<Post>(posts, (int)totalPostCount); // This might need tweaking if the application grows
+        }
+
         public void OpenNewPostForm()
         {
             NewPostForm.OpenDialog();
