24fc11b1057604b96009f582f140247f30ee0a4f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-17 23:40:57 +0200

    Refactored posts to be rendered in a dedicated component.

---
+++ FlowUI/Components/PostCardComponent.razor.cs
@@ -0,0 +1,27 @@
+﻿using Flow.Core.DomainModels;
+using Flow.Core.Mediate.UserQuery;
+using MediatR;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System.Threading.Tasks;
+
+namespace FlowUI.Components
+{
+    public partial class PostCardComponent
+    {
+        [Parameter]
+        public Post Post { get; set; }
+
+        public User PostOwner { get; set; }
+
+        [Inject]
+        protected IMediator _mediator { get; set; }
+        [CascadingParameter]
+        protected Task<AuthenticationState> _authenticationStateTask { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            PostOwner = await _mediator.Send(new GetUserByIdRequest { UserId = Post.PostOwnerId });
+        }
+    }
+}
