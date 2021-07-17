ae35370acc559502ee4edc089b86234154d0b7c0
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-17 11:16:28 +0200

    Added request and request handler for inserting posts in the DB.

---
+++ Flow.Core/Mediate/InsertPost/InsertPostRequest.cs
@@ -0,0 +1,10 @@
+﻿using Flow.Core.DomainModels;
+using MediatR;
+
+namespace Flow.Core.Mediate.InsertPost
+{
+    public class InsertPostRequest : IRequest
+    {
+        public Post Post { get; set; }
+    }
+}
---
+++ Flow.Core/Mediate/InsertPost/InsertPostRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using Flow.Core.Contracts;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flow.Core.Mediate.InsertPost
+{
+    public class InsertPostRequestHandler : IRequestHandler<InsertPostRequest>
+    {
+        private readonly IPostRepository _repository;
+
+        public InsertPostRequestHandler(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Unit> Handle(InsertPostRequest request, CancellationToken cancellationToken)
+        {
+            await _repository.UpsertPost(request.Post);
+            return Unit.Value;
+        }
+    }
+}
