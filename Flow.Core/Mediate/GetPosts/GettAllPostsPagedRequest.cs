494e35ba18bd0b202b994c1fec2c40ffcbeb5001
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-17 15:35:25 +0200

    Added a request to retreive all posts in the database, with paging support.

---
+++ Flow.Core/Mediate/GetPosts/GettAllPostsPagedRequest.cs
@@ -0,0 +1,12 @@
+﻿using Flow.Core.DomainModels;
+using MediatR;
+using System.Collections.Generic;
+
+namespace Flow.Core.Mediate.GetPosts
+{
+    public class GettAllPostsPagedRequest : IRequest<ICollection<Post>>
+    {
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
---
+++ Flow.Core/Mediate/GetPosts/GettAllPostsPagedRequestHandler.cs
@@ -0,0 +1,24 @@
+﻿using Flow.Core.Contracts;
+using Flow.Core.DomainModels;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Flow.Core.Mediate.GetPosts
+{
+    public class GettAllPostsPagedRequestHandler : IRequestHandler<GettAllPostsPagedRequest, ICollection<Post>>
+    {
+        private readonly IPostRepository _repository;
+
+        public GettAllPostsPagedRequestHandler(IPostRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ICollection<Post>> Handle(GettAllPostsPagedRequest request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetPosts(request.Skip, request.Take);
+        }
+    }
+}
