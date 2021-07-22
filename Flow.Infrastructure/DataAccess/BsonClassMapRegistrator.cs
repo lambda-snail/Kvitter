abebfcd67c192c32e73ecbf5bb946fff48d1ec0f
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-22 21:41:52 +0200

    Refactored to allow sharing of class maps between application and tests.

---
+++ Flow.Infrastructure/DataAccess/BsonClassMapRegistrator.cs
@@ -0,0 +1,37 @@
+﻿using Flow.Core.DomainModels;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.IdGenerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flow.Infrastructure.DataAccess
+{
+    public static class BsonClassMapRegistrator
+    {
+        public static void RegisterBsonClassMaps()
+        {
+            if (!BsonClassMap.IsClassMapRegistered(typeof(User)))
+            {
+                BsonClassMap.RegisterClassMap<User>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.MapIdProperty(user => user.UserId);
+                });
+            }
+
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Post)))
+            {
+                BsonClassMap.RegisterClassMap<Post>(cm =>
+                {
+                    cm.AutoMap();
+                    cm.SetIdMember(cm.GetMemberMap(post => post.PostId));
+                    cm.IdMemberMap.SetIgnoreIfDefault(true);
+                    cm.IdMemberMap.SetIdGenerator(CombGuidGenerator.Instance);
+                });
+            }
+        }
+    }
+}
