a40af474f835f91ae44bc750e5294c100b9ddfae
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-07-23 22:29:28 +0200

    Added request to get the id of the logged in user.

---
+++ FlowUI/Utilities/LoggedInUserRequest/GetIdLoggedInUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+
+namespace FlowUI.Utilities.LoggedInUserRequest
+{
+    /**
+     * A request for the Id of the currently logged in user. This is intended to be used when only the id is needed,
+     * which will save one trip to the database.
+     */
+    public class GetIdLoggedInUserRequest : IRequest<Guid> {}
+}
