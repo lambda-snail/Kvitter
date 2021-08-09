e5a73f3a97a467a2e88c497f31fe580e6bb2f35d
Niclas Blomberg <79707893+lambda-snail@users.noreply.github.com>
2021-08-09 15:37:20 +0200

    Created and styled a friends list.

---
+++ FlowUI/Components/FriendsListComponent.razor.cs
@@ -0,0 +1,14 @@
+﻿using Flow.Core.DomainModels;
+using Microsoft.AspNetCore.Components;
+
+namespace FlowUI.Components
+{
+    /**
+     * Displays a list of the friends of a given user.
+     */
+    public partial class FriendsListComponent
+    {
+        [Parameter]
+        public User User { get; set; }
+    }
+}
